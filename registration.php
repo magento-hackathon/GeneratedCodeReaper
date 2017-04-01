d780f6a6ff7a2913f98700fdf520beeb28eb340a
Andreas von Studnitz <avs@integer-net.de>
2017-04-01 22:35:44 +0200

    Add module base structure

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Frkamework\Component\ComponentRegistrar::MODULE,
+    'Hackathon_GeneratedCodeReaper',
+    __DIR__
+);
