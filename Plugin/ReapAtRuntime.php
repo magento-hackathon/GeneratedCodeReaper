aaa9a743ff3ee5c6bd1640f510f39e23efdc4bb8
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2017-04-01 19:41:06 -0700

    add a runtime config

--- Plugin/ReapAtRuntime.php
+++ Plugin/ReapAtRuntime.php
@@ -3,18 +3,32 @@
 namespace Hackathon\GeneratedCodeReaper\Plugin;
 
 use Magento\Framework\ObjectManager\ConfigLoaderInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class ReapAtRuntime
 {
 
+    const XML_PATH_REAP_AT_RUNTIME = 'dev/static/reaper_at_runtime';
+
     private $hasReaped = false;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
 
     public function beforeLoad(
         ConfigLoaderInterface $subject,
         $requestedType,
         array $arguments = []
     ) {
-        if (!$this->hasReaped) {
+        if ($this->scopeConfig->getValue(self::XML_PATH_REAP_AT_RUNTIME) && !$this->hasReaped) {
             $this->runReaper();
         }
 
